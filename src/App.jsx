498090cf7efacebd500fefef23ce3fca474e69fb
Dhev <github@dhev.anonaddy.me>
2024-06-12 07:50:48 +0530

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hi</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
