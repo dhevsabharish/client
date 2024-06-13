26e5ac9e209544b0a27a6c63734fe82bbba62285
Dhev <github@dhev.anonaddy.me>
2024-06-13 06:55:37 +0530

    Token validity check

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import axios from "axios";
 
 export const ProtectedRoute = ({ children }) => {
-    const { token } = useAuth();
-    if (!token) {
-        // user is not authenticated
+    const { token, logout } = useAuth();
+    const [isTokenValid, setIsTokenValid] = useState(true);
+
+    useEffect(() => {
+        const verifyToken = async () => {
+            try {
+                await axios.post("http://localhost:3000/verify_token", null, {
+                    headers: { Authorization: token },
+                });
+            } catch (error) {
+                console.error("Token verification failed:", error);
+                logout();
+                setIsTokenValid(false);
+            }
+        };
+
+        if (token) {
+            verifyToken();
+        } else {
+            setIsTokenValid(false);
+        }
+    }, [token, logout]);
+
+    if (!isTokenValid) {
+        // Token is invalid or not present
         return <Navigate to="/login" />;
     }
+
     return children;
 };
