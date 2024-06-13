50243b73b351e12e5ec811620e9defa31b279ff2
Dhev <github@dhev.anonaddy.me>
2024-06-13 07:30:12 +0530

    Update Admin Page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Typography } from "@mui/material";
 import { useAuth } from "../hooks/useAuth";
+import { LibrarianDashboard } from "./LibrarianDashboard";
 
 export const AdminHome = () => {
     const { logout } = useAuth();
@@ -11,16 +12,25 @@ export const AdminHome = () => {
     return (
         <Box
             display="flex"
-            justifyContent="space-between"
+            flexDirection="column"
             alignItems="center"
             p={2}
         >
-            <Typography variant="h4" component="h1">
-                Admin Home
-            </Typography>
-            <Button onClick={handleLogout} variant="contained">
-                Logout
-            </Button>
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                width="100%"
+                mb={2}
+            >
+                <Typography variant="h4" component="h1">
+                    Admin Home
+                </Typography>
+                <Button onClick={handleLogout} variant="contained">
+                    Logout
+                </Button>
+            </Box>
+            <LibrarianDashboard />
         </Box>
     );
 };
